ade78f91efb5c03f6803c7f1fa4222181482c336
akannur <akannur@qwinixtech.com>
2014-10-29 10:55:51 +0530

    add errors chapter

---
+++ Chapter 09 - Errors/1-error_def.go
@@ -0,0 +1,44 @@
+/*  An error is anything that can describe itself as an error string. The idea is captured by the predefined, built-in interface type, error, with its single method, Error, returning a string:
+
+    type error interface {
+         Error() string
+    }
+
+    The fmt package's various print routines automatically know to call the method when asked to print an error.   */
+
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type MyError struct {
+    When time.Time
+    What string
+}
+
+func (e *MyError) Error() string {
+    return fmt.Sprintf("at %v, %s",
+        e.When, e.What)
+}
+
+func run() error {
+    return &MyError{
+        time.Now(),
+        "it didn't work",
+    }
+}
+
+func main() {
+    if err := run(); err != nil {
+        fmt.Println(err)
+    }
+}
+
+
+
+/******OUTPUT******
+at 2009-11-10 23:00:00 +0000 UTC, it didn't work 
+*/
